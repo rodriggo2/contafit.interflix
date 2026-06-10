451f7cb2602ff658461257ab4b4be2ee7ccf8bd6
rodriggo2 <rodriggo2@gmail.com>
2026-06-10 18:46:45 -0300

    Register FormularioNFSe component with Makeswift

---
+++ components/FormularioNFSe.makeswift.ts
@@ -0,0 +1,9 @@
+// components/FormularioNFSe.makeswift.ts
+import { Makeswift } from '@makeswift/runtime/next'
+import { FormularioNFSe } from './FormularioNFSe'
+
+Makeswift.registerComponent(FormularioNFSe, {
+  type: 'formulario-nfse',
+  label: 'Contafit / Emissor NFS-e',
+  props: {},
+})
