e0faf9871a6ff0b3305f3355f777883e0a7dbfb0
rodriggo2 <rodriggo2@gmail.com>
2026-06-10 18:44:39 -0300

    Add FormularioNFSe component import

--- lib/makeswift/components.ts
+++ lib/makeswift/components.ts
@@ -1,3 +1,4 @@
 import '@/components/Accordions/Accordions.makeswift'
 import '@/components/Marquee/Marquee.makeswift'
 import '@/components/Tabs/Tabs.makeswift'
+import '@/components/FormularioNFSe.makeswift'
