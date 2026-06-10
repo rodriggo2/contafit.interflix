137312c1b14cab0ccf4d2629db5eaba2eadca863
rodriggo2 <rodriggo2@gmail.com>
2026-06-10 18:59:56 -0300

    Update FormularioNFSe.makeswift.ts

--- components/FormularioNFSe.makeswift.ts
+++ components/FormularioNFSe.makeswift.ts
@@ -1,14 +1,10 @@
 // components/FormularioNFSe.makeswift.ts
-import { Makeswift } from '@makeswift/runtime/next'
+import { makeswift } from '@/lib/makeswift/client'
 import { FormularioNFSe } from './FormularioNFSe'
 
-// Forma alternativa de registro
-const client = new Makeswift(process.env.MAKESWIFT_API_KEY!)
-
-client.registerComponent(FormularioNFSe, {
+// @ts-ignore
+makeswift.registerComponent(FormularioNFSe, {
   type: 'formulario-nfse',
-  label: 'ContaFit / Emissor NFS-e',
-  props: {}
+  label: 'Contafit / Emissor NFS-e',
+  props: {},
 })
-
-export { client }
