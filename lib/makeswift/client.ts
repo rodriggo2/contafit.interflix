ceb60699948049c1205bd420558f7af93d3e4dea
rodriggo2 <rodriggo2@gmail.com>
2026-06-26 11:29:39 -0300

    Fix export order of Makeswift instance

--- lib/makeswift/client.ts
+++ lib/makeswift/client.ts
@@ -1,15 +1,12 @@
-// lib/makeswift/client.ts
 import { Makeswift } from '@makeswift/runtime/next'
 import { strict } from 'assert'
 import { runtime } from './runtime'
 
 strict(process.env.MAKESWIFT_SITE_API_KEY, 'MAKESWIFT_SITE_API_KEY is required')
 
-// Criamos a instância
 const makeswiftInstance = new Makeswift(process.env.MAKESWIFT_SITE_API_KEY, {
   runtime,
 })
 
-// Exportamos com os dois nomes para não quebrar nenhum arquivo do projeto
 export const client = makeswiftInstance
 export const makeswift = makeswiftInstance
