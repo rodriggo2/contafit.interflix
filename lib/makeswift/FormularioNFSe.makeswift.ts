2c7c154464a57f68d357db46a83fedc6b56f1f7f
rodriggo2 <rodriggo2@gmail.com>
2026-06-26 11:31:54 -0300

    Update Makeswift component registration syntax

--- lib/makeswift/FormularioNFSe.makeswift.ts
+++ lib/makeswift/FormularioNFSe.makeswift.ts
@@ -1,7 +1,8 @@
-import { Makeswift } from '@makeswift/runtime/next'
+// lib/makeswift/FormularioNFSe.makeswift.ts
+import { makeswift } from './client'  // ou '../lib/makeswift/client' se necessário
 import { FormularioNFSe } from '../../components/FormularioNFSe' 
 
-Makeswift.registerComponent(FormularioNFSe, {
+makeswift.registerComponent(FormularioNFSe, {
   type: 'formulario-nfse',
   label: 'Contafit / Emissor NFS-e',
   props: {},
