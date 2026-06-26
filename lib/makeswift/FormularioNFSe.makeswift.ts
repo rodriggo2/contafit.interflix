643757e961794dca7a11c33468fc84abfaafb153
rodriggo2 <rodriggo2@gmail.com>
2026-06-26 11:37:01 -0300

    Delete lib/makeswift/FormularioNFSe.makeswift.ts

--- lib/makeswift/FormularioNFSe.makeswift.ts
+++
@@ -1,9 +0,0 @@
-// lib/makeswift/FormularioNFSe.makeswift.ts
-import { makeswift } from './client'   // ou '../lib/makeswift/client' se necessário
-import { FormularioNFSe } from '../../components/FormularioNFSe' 
-
-makeswift.registerComponent(FormularioNFSe, {
-  type: 'formulario-nfse',
-  label: 'Contafit / Emissor NFS-e',
-  props: {},
-})
