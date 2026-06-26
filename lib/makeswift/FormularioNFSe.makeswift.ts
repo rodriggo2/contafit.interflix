9a2ee20ccddf5515f0dd7201ed51f1506a12ce49
rodriggo2 <rodriggo2@gmail.com>
2026-06-26 11:23:37 -0300

    Fix import path for FormularioNFSe component

--- lib/makeswift/FormularioNFSe.makeswift.ts
+++ lib/makeswift/FormularioNFSe.makeswift.ts
@@ -1,6 +1,8 @@
-// components/FormularioNFSe.makeswift.ts
+// lib/makeswift/FormularioNFSe.makeswift.ts
 import { Makeswift } from '@makeswift/runtime/next'
-import { FormularioNFSe } from './FormularioNFSe'
+// MUDANÇA AQUI: O caminho provavelmente deve apontar para a pasta components
+// Ajuste para '../../components/FormularioNFSe' se ele estiver lá
+import { FormularioNFSe } from '../../components/FormularioNFSe' 
 
 Makeswift.registerComponent(FormularioNFSe, {
   type: 'formulario-nfse',
